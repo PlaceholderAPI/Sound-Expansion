e343b718eb1eed25082d5ca26ae3f97c29cd3f49
Andre601 <11576465+Andre601@users.noreply.github.com>
2021-08-04 18:44:59 +0200

    Update Sound-Expansion

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Sound-Expansion
+The Sound-Expansion allows you to play a Minecraft sound to a single player or the entire server.
+
+## Placeholders
+
+### `%sound_<sound>-<volume>-<pitch>%`
+Plays a sound to the player.  
+Both `<volume>` and `<pitch>` are optional and default to 10 (full volume) and 1 (normal pitch) respectively.
+
+### `%sound_all_<sound>-<volume>-<pitch>%`
+Plays a sound to all players on the server.  
+Both `<volume>` and `<pitch>` are optional and default to 10 (full volume) and 1 (normal pitch) respectively.
--- src/com/extendedclip/papi/expansion/sound/SoundExpansion.java
+++ src/com/extendedclip/papi/expansion/sound/SoundExpansion.java
@@ -2,6 +2,7 @@ package com.extendedclip.papi.expansion.sound;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
 
 import me.clip.placeholderapi.expansion.Cacheable;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
@@ -10,33 +11,29 @@ import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nonnull;
+
 public class SoundExpansion extends PlaceholderExpansion implements Cacheable {
 
-	private final Map<String, CachedSound> sounds = new ConcurrentHashMap<String, CachedSound>();
-	
-	@Override
-	public boolean canRegister() {
-		return true;
-	}
+	private final Map<String, CachedSound> sounds = new ConcurrentHashMap<>();
+	private final String VERSION = getClass().getPackage().getImplementationVersion();
 
 	@Override
+	@Nonnull
 	public String getAuthor() {
 		return "clip";
 	}
 
 	@Override
+	@Nonnull
 	public String getIdentifier() {
 		return "sound";
 	}
 
 	@Override
-	public String getPlugin() {
-		return null;
-	}
-
-	@Override
+	@Nonnull
 	public String getVersion() {
-		return "1.0.0";
+		return VERSION;
 	}
 
 	@Override
@@ -45,7 +42,7 @@ public class SoundExpansion extends PlaceholderExpansion implements Cacheable {
 	}
 
 	@Override
-	public String onPlaceholderRequest(Player p, String identifier) {
+	public String onPlaceholderRequest(Player p, @Nonnull String identifier) {
 		
 		if (p == null) {
 			return "";
@@ -55,7 +52,7 @@ public class SoundExpansion extends PlaceholderExpansion implements Cacheable {
 		
 		if (identifier.startsWith("all_")) {
 			announce = true;
-			identifier = identifier.replace("all_", "");
+			identifier = identifier.replaceFirst(Pattern.quote("all_"), "");
 		}
 		
 		if (sounds.containsKey(identifier)) {
@@ -69,7 +66,7 @@ public class SoundExpansion extends PlaceholderExpansion implements Cacheable {
 		
 		int first = identifier.indexOf("-");
 		
-		Sound s = null;
+		Sound s;
 		
 		if (first == -1) {
 			try {
@@ -95,13 +92,9 @@ public class SoundExpansion extends PlaceholderExpansion implements Cacheable {
 			}
 		}
 		
-		if (s == null) {
-			return null;
-		}
-
-		int volume = 10;
+		float volume = 10;
 		
-		int pitch = 1;
+		float pitch = 1;
 		
 		String volPitch = identifier.substring(first+1);
 		
@@ -109,14 +102,12 @@ public class SoundExpansion extends PlaceholderExpansion implements Cacheable {
 		
 		if (second >= 1) {
 			try {
-				volume = Integer.parseInt(volPitch.substring(0, second));
-			} catch (Exception ex) {
-			}
+				volume = Float.parseFloat(volPitch.substring(0, second));
+			} catch (Exception ignored) {}
 			
 			try {
-				pitch = Integer.parseInt(volPitch.substring(second+1));
-			} catch (Exception ex) {
-			}
+				pitch = Float.parseFloat(volPitch.substring(second+1));
+			} catch (Exception ignored) {}
 		}
 		
 		CachedSound cs = new CachedSound(s, volume, pitch);
@@ -129,13 +120,13 @@ public class SoundExpansion extends PlaceholderExpansion implements Cacheable {
 		return "";
 	}
 
-	private class CachedSound {
+	private static class CachedSound {
 
-		private Sound s;
-		private int volume;
-		private int pitch;
+		private final Sound s;
+		private final float volume;
+		private final float pitch;
 		
-		public CachedSound(Sound s, int volume, int pitch) {
+		public CachedSound(Sound s, float volume, float pitch) {
 			this.s = s;
 			this.volume = volume;
 			this.pitch = pitch;
@@ -146,6 +137,7 @@ public class SoundExpansion extends PlaceholderExpansion implements Cacheable {
 				play(p);
 			}
 		}
+		
 		public void play(Player p) {
 			p.playSound(p.getLocation(), s, volume, pitch);
 		}
